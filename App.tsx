27ada8edc5eb35f0f8abe309fb9fa091099ef902
joaocarrato <joaocarrato18@gmail.com>
2025-08-27 19:07:41 -0300

    feat: add text component with custom settings

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { ThemeProvider } from '@shopify/restyle';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { Text } from '@components';
 import { theme } from '@theme';
 
 function App() {
@@ -10,12 +11,7 @@ function App() {
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
         <View style={{ marginTop: 50, marginHorizontal: 20 }}>
-          <Text style={{ fontSize: 20 }}>Olá</Text>
-          <Text
-            style={{ fontSize: 20, fontFamily: 'Roboto Condensed SemiBold' }}
-          >
-            Olá
-          </Text>
+          <Text>Olá</Text>
         </View>
       </ThemeProvider>
     </SafeAreaProvider>
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { TextStyle } from 'react-native';
+
+import { createText } from '@shopify/restyle';
+
+import { Theme } from '@theme';
+
+const SRText = createText<Theme>();
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+export interface TextProps extends SRTextProps {
+  preset?: TextVariants;
+  semiBold?: boolean;
+  bold?: boolean;
+}
+
+export function Text({
+  children,
+  preset = 'textLg',
+  semiBold,
+  bold,
+  style,
+  ...srTextProps
+}: TextProps) {
+  const fontFamily = getFontFamily(semiBold, bold);
+
+  return (
+    <SRText
+      style={[$fontSizes[preset], { fontFamily }, style]}
+      {...srTextProps}
+    >
+      {children}
+    </SRText>
+  );
+}
+
+function getFontFamily(semiBold?: boolean, bold?: boolean) {
+  switch (true) {
+    case bold:
+      return $fontVariants.bold;
+    case semiBold:
+      return $fontVariants.semiBold;
+    default:
+      return $fontVariants.regular;
+  }
+}
+
+type TextVariants =
+  | 'text2xl'
+  | 'textXl'
+  | 'textLg'
+  | 'textBase'
+  | 'textSm'
+  | 'textXs'
+  | 'headline';
+
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  text2xl: { fontSize: 24, lineHeight: 31 },
+  textXl: { fontSize: 20, lineHeight: 28 },
+  textLg: { fontSize: 18, lineHeight: 29 },
+  textBase: { fontSize: 16, lineHeight: 26 },
+  textSm: { fontSize: 14, lineHeight: 22 },
+  textXs: { fontSize: 12, lineHeight: 19 },
+  headline: { fontSize: 12, lineHeight: 14 },
+};
+
+const $fontVariants = {
+  black: 'Roboto Condensed Black',
+  bold: 'Roboto Condensed Bold',
+  italic: 'Roboto Condensed Italic',
+  medium: 'Roboto Condensed Medium',
+  regular: 'Roboto Condensed Regular',
+  semiBold: 'Roboto Condensed SemiBold',
+};
